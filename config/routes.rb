d1c804057d93bedb189139e311080da43111dfa3
kittanutp <kittanut.pong@gmail.com>
2023-06-12 22:55:14 +0700

    [add] family member

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'task/task:string'
+  get 'task/status:enum'
+  get 'task/member_id:integet:index'
+  resources :family_members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
