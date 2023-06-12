afb7b29f021692581a4437804aed2b473d07fc76
kittanutp <kittanut.pong@gmail.com>
2023-06-12 22:58:46 +0700

    [remove] unused

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'task/task:string'
-  get 'task/status:enum'
-  get 'task/member_id:integet:index'
   resources :family_members
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
